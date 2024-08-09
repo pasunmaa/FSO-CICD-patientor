4b823d4fac5018e954c87bce734cf1dfe11501ce
Petri Asunmaa <petri.asunmaa@outlook.com>
2024-08-09 16:23:04 +0300

    11.20 linting added + run server with shell script

--- build_render.sh
+++ build_render.sh
@@ -7,12 +7,15 @@ npm install
 echo "Install dependencies on client folder"
 cd client
 npm install
+echo "Lint client"
+npm run lint
 echo "Build client"
 npm run build
 echo "Install dependencies on server folder"
 cd ../server
 npm install
+echo "Lint server"
+npm run lint
 echo "Build server"
 npm run tsc
 cd ..
-
---
+++ run_render.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting production server"
+
+cd server
+npm start
