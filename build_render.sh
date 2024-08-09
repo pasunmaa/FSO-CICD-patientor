737f27708fa0b9c5c7f687ad1056cb951b629921
Petri Asunmaa <petri.asunmaa@outlook.com>
2024-08-09 15:05:35 +0300

    11.20 more echos in build_render.sh, and do not ping Discord unless asked in commit

--- build_render.sh
+++ build_render.sh
@@ -2,12 +2,17 @@
 
 echo "Patientor monorepo build script for render"
 echo "The current NODE_ENV is: $NODE_ENV"
+echo "Install dependencies on root folder"
 npm install
+echo "Install dependencies on client folder"
 cd client
 npm install
+echo "Build client"
 npm run build
+echo "Install dependencies on server folder"
 cd ../server
 npm install
+echo "Build server"
 npm run build
 cd ..
 
