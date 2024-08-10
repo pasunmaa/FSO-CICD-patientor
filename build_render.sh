15878f27c43da9c90807ace30c1f8047616b2190
Petri Asunmaa <petri.asunmaa@outlook.com>
2024-08-10 12:48:20 +0300

    11.20 tee logging added to build script

--- build_render.sh
+++ build_render.sh
@@ -4,9 +4,11 @@ echo "Patientor monorepo build script for render"
 echo "The current NODE_ENV is: $NODE_ENV"
 echo "Install dependencies on root folder"
 npm install
+npm ls | tee npm_ls_root.txt
 echo "Install dependencies on client folder"
 cd client
 npm install
+npm ls | tee npm_ls_client.txt
 echo "Lint client"
 npm run lint
 echo "Build client"
@@ -14,8 +16,9 @@ npm run build
 echo "Install dependencies on server folder"
 cd ../server
 npm install
+npm ls | tee npm_ls_server.txt
 echo "Lint server"
 npm run lint
 echo "Build server"
-npm run tsc
+npm run tsc | tee tsc_server.txt
 cd ..
