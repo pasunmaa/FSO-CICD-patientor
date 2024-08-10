40abd86a66adc41344e0fb1dcfea7d067197d348
Petri Asunmaa <petri.asunmaa@outlook.com>
2024-08-10 14:11:14 +0300

    11.20 pipeline cleaning and build script logging additions

--- build_render.sh
+++ build_render.sh
@@ -4,11 +4,11 @@ echo "Patientor monorepo build script for render"
 echo "The current NODE_ENV is: $NODE_ENV"
 echo "Install dependencies on root folder"
 npm install --include dev
-npm ls | tee npm_ls_root.txt
+npm ls --include prod --include dev  | tee npm_ls_root.txt
 echo "Install dependencies on client folder"
 cd client
 npm install --include prod --include dev
-npm ls | tee npm_ls_client.txt
+npm ls --include prod --include dev | tee npm_ls_client.txt
 echo "Lint client"
 npm run lint
 echo "Build client"
@@ -16,7 +16,7 @@ npm run build
 echo "Install dependencies on server folder"
 cd ../server
 npm install --include prod --include dev
-npm ls | tee npm_ls_server.txt
+npm ls --include prod --include dev | tee npm_ls_server.txt
 echo "Lint server"
 npm run lint
 echo "Build server"
